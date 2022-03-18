a7f2eb41291a9f08ecebc7affa0383d0d49e7209
Chavin2543 <rungallday@hotmail.com>
2022-03-19 00:07:35 +0700

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by chavin Panicharoen on 19/3/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
